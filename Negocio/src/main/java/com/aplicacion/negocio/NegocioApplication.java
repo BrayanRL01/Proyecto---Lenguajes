dc3846993402dae806b5b38c356cc786d7d4538f
marvcs <marv0712@gmail.com>
2022-11-07 19:13:24 -0600

    Lectura con sobrecarga funcionando

--- Negocio/src/main/java/com/aplicacion/negocio/NegocioApplication.java
+++ Negocio/src/main/java/com/aplicacion/negocio/NegocioApplication.java
@@ -2,6 +2,7 @@ package com.aplicacion.negocio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class NegocioApplication {
@@ -9,5 +10,4 @@ public class NegocioApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(NegocioApplication.class, args);
 	}
-
 }
