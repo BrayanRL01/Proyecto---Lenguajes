1967702d3d37f22b69d822c31742548826046791
BrayanRL01 <brayan12345303@gmail.com>
2022-11-15 17:09:12 -0600

    Cambios de clases.

--- Negocio/src/main/java/com/aplicacion/negocio/controller/EmpleadosController.java
+++ Negocio/src/main/java/com/aplicacion/negocio/controller/EmpleadosController.java
@@ -3,12 +3,9 @@ package com.aplicacion.negocio.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.aplicacion.negocio.entity.Empleados;
 import com.aplicacion.negocio.service.EmpleadosService;
--- Negocio/src/main/java/com/aplicacion/negocio/controller/PersonasController.java
+++ Negocio/src/main/java/com/aplicacion/negocio/controller/PersonasController.java
@@ -3,23 +3,23 @@ package com.aplicacion.negocio.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.aplicacion.negocio.entity.Personas;
 import com.aplicacion.negocio.service.PersonasService;
 
-@RestController
+@Controller
 public class PersonasController {
 
     @Autowired
     PersonasService PS;
 
     @GetMapping("/listapersonas")
-    public ResponseEntity<List<Personas>> ListaPersonas() {
+    public String Index(Model M) {
         List<Personas> ListaPersonas = PS.ListaPersonas();
-        return new ResponseEntity(ListaPersonas, HttpStatus.OK);
+        M.addAttribute("TAB_PERSONAS", ListaPersonas);
+        return "listapersonas";
     }
 }
